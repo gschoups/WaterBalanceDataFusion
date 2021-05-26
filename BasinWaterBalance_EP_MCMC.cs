6260e336e8f8e516d3d84500d40c6f5882dab57f
Gerrit Schoups <gerrit.schoups@gmail.com>
2021-05-26 18:50:48 +0200

    Add project files.

---
+++ BasinWaterBalance_EP_MCMC.cs
@@ -0,0 +1,355 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+using Microsoft.ML.Probabilistic;
+using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Models;
+
+namespace BasinWaterBalance
+{
+    using GaussianArrayEstimator = ArrayEstimator<GaussianEstimator, DistributionStructArray<Gaussian, double>, Gaussian>;
+    using Range = Microsoft.ML.Probabilistic.Models.Range;
+
+    [Serializable]
+    public partial class BasinWaterBalance_EP : IGeneratedAlgorithm
+    {
+        public int NumberOfIterationsEP { get; set; } = 3;
+        public int NumberOfIterationsPerParameterMCMC { get; set; } = 2000;
+        public VectorGaussian JointParameterPosterior { get; private set; }
+
+        /// <summary>
+        /// Saves the current model to a file.
+        /// </summary>
+        /// <param name="modelFile">Where to save the results.</param>
+        public void SaveAs(string modelFile)
+        {
+            string path = Path.GetDirectoryName(modelFile);
+            if (!string.IsNullOrEmpty(path)) Directory.CreateDirectory(path);
+            var serializer = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new FileStream(modelFile, FileMode.Create))
+            {
+                serializer.Serialize(stream, this);
+            }
+        }
+
+        /// <summary>
+        /// Loads a model from a file.
+        /// </summary>
+        /// <param name="modelFile">The file.</param>
+        /// <returns>The model.</returns>
+        public static BasinWaterBalance_EP Load(string modelFile)
+        {
+            var serializer = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new FileStream(modelFile, FileMode.Open))
+            {
+                return (BasinWaterBalance_EP)serializer.Deserialize(stream);
+            }
+        }
+
+        /// <summary>
+        /// Runs MCMC and computes posteriors of error parameters and water balance variables.
+        /// </summary>
+        /// <returns>Largest value of the unnormalized log parameter posterior density.</returns>
+        public double InferMCMC()
+        {
+            int nIter = NumberOfIterationsPerParameterMCMC * parPrior.Dimension;
+            int nInitial = 100 * parPrior.Dimension;
+            var samples = new List<Vector>(nIter + nInitial);
+            var logProbs = new List<double>(nIter + nInitial);
+            for (int i = 0; i < nInitial; i++)
+            {
+                var par = parPrior.Sample();
+                samples.Add(par);
+                logProbs.Add(GetLogProb(par));
+            }
+            MCMC.DEMC_Z(GetLogProb, nIter, samples, logProbs);
+            EstimateLatentMarginals(samples);
+            int mode = logProbs.IndexOfMax();
+            return logProbs[mode];
+        }
+
+        /// <summary>
+        /// Evaluates the unnormalized log parameter posterior (Eq. 17) at a specific error parameter vector.
+        /// </summary>
+        /// <param name="par">The error parameter vector.</param>
+        /// <returns>The unnormalized log parameter posterior density.</returns>
+        private double GetLogProb(Vector par)
+        {
+            Par = par;
+            Execute(NumberOfIterationsEP);
+            return EvidenceMarginal().LogOdds;
+        }
+
+        /// <summary>
+        /// Computes error parameter and water balance posteriors (Eq. 19) given a list of error parameter vectors sampled from the posterior.
+        /// </summary>
+        /// <param name="pars">The error parameter vectors.</param>
+        private void EstimateLatentMarginals(List<Vector> pars)
+        {
+            var estPar = new VectorGaussianEstimator(parPrior.Dimension);
+
+            var estwE = new BetaEstimator();
+            var estfE = new GammaEstimator();
+            var estrE = new BetaEstimator();
+            var estA = new GammaEstimator();
+            var estDelta = new BetaEstimator();
+            var estSStd = new GammaEstimator();
+            var estaQ = new GammaEstimator();
+            var estbQ = new GammaEstimator();
+            var estwP = new BetaEstimator();
+            var estrP = new BetaEstimator();
+
+            var estP = new GaussianArrayEstimator(nt, i => new GaussianEstimator());
+            var estE = new GaussianArrayEstimator(nt, i => new GaussianEstimator());
+            var estQ = new GaussianArrayEstimator(nt, i => new GaussianEstimator());
+            var estS = new GaussianArrayEstimator(nt, i => new GaussianEstimator());
+            var estS0 = new GaussianEstimator();
+
+            double nLast = 0.2 * pars.Count;//compute posterior from last 20% samples
+            for (int i = 0; i < nLast; i++)
+            {
+                int index = i;// Rand.Int(0, 1000);
+                Par = pars[pars.Count - 1 - index];
+
+                Execute(NumberOfIterationsEP);
+
+                estPar.Add(Par);
+
+                estwE.Add(wE_marginal_F);
+                estfE.Add(fE_marginal_F);
+                estrE.Add(rE_marginal_F);
+                estA.Add(A_marginal_F);
+                estDelta.Add(Delta_marginal_F);
+                estSStd.Add(SStd_marginal_F);
+                estaQ.Add(aQ_marginal_F);
+                estbQ.Add(bQ_marginal_F);
+                estwP.Add(wP_marginal_F);
+                estrP.Add(rP_marginal_F);
+
+                estP.Add(P_marginal_F);
+                estE.Add(E_marginal_F);
+                estQ.Add(Q_marginal_F);
+                estS.Add(S_marginal_F);
+                estS0.Add(S0_marginal_F);
+            }
+
+            JointParameterPosterior = estPar.GetDistribution(new VectorGaussian(parPrior.Dimension));
+
+            wE_marginal_F = estwE.GetDistribution(wE_marginal_F);
+            fE_marginal_F = estfE.GetDistribution(fE_marginal_F);
+            rE_marginal_F = estrE.GetDistribution(rE_marginal_F);
+            A_marginal_F = estA.GetDistribution(A_marginal_F);
+            Delta_marginal_F = estDelta.GetDistribution(Delta_marginal_F);
+            SStd_marginal_F = estSStd.GetDistribution(SStd_marginal_F);
+            aQ_marginal_F = estaQ.GetDistribution(aQ_marginal_F);
+            bQ_marginal_F = estbQ.GetDistribution(bQ_marginal_F);
+            wP_marginal_F = estwP.GetDistribution(wP_marginal_F);
+            rP_marginal_F = estrP.GetDistribution(rP_marginal_F);
+
+            P_marginal_F = estP.GetDistribution(P_marginal_F);
+            E_marginal_F = estE.GetDistribution(E_marginal_F);
+            Q_marginal_F = estQ.GetDistribution(Q_marginal_F);
+            S_marginal_F = estS.GetDistribution(S_marginal_F);
+            S0_marginal_F = estS0.GetDistribution(S0_marginal_F);
+        }
+
+        /// <summary>
+        /// Generates EP inference code for computing conditional water balance posteriors.
+        /// </summary>
+        public static void GenerateCode()
+        {
+            // Dummy input values
+            int ntValue = 1;
+            var value = new double[ntValue];
+            var parPriorValue = new VectorGaussian(Vector.Zero(10), PositiveDefiniteMatrix.Identity(10));
+            var s0PriorValue = new Gaussian(0, 1);
+
+            // Define variables
+            Variable<bool> evidence = Variable.Bernoulli(0.5).Named("evidence");
+            Variable<int> nt = Variable.Observed(ntValue).Named("Nt");
+            Range timeInterval = new Range(nt);
+            Variable<VectorGaussian> parPrior;
+            Variable<Gaussian> S0Prior;
+            VariableArray<double> PObs1, PObs2, PStd, EObs1, EObs2, QObs, QObsVar, SObs;
+            VariableArray<double> P, E, Q, S;
+            Variable<double> S0, wE, fE, rE, A, Delta, SStd, aQ, bQ, wP, rP;
+            Variable<Vector> par;
+
+            // Define model
+            using (Variable.If(evidence))
+            {
+                // Parameters
+                parPrior = Variable.Observed(parPriorValue).Named("ParPrior");
+                par = Variable.Random<Vector, VectorGaussian>(parPrior).Named("Par");
+                wE = Variable.Logistic(Variable.GetItem(par, 0)).Named("wE");
+                fE = Variable.Exp(Variable.GetItem(par, 1)).Named("fE");
+                rE = Variable.Logistic(Variable.GetItem(par, 2)).Named("rE");
+                A = Variable.Exp(Variable.GetItem(par, 3)).Named("A");
+                Delta = Variable.Logistic(Variable.GetItem(par, 4)).Named("Delta");
+                SStd = Variable.Exp(Variable.GetItem(par, 5)).Named("SStd");
+                aQ = Variable.Exp(Variable.GetItem(par, 6)).Named("aQ");
+                bQ = Variable.Exp(Variable.GetItem(par, 7)).Named("bQ");
+                wP = Variable.Logistic(Variable.GetItem(par, 8)).Named("wP");
+                rP = Variable.Logistic(Variable.GetItem(par, 9)).Named("rP");
+
+                // Observations
+                PObs1 = Variable.Observed(value, timeInterval).Named("PObs1");
+                PObs2 = Variable.Observed(value, timeInterval).Named("PObs2");
+                PStd = Variable.Observed(value, timeInterval).Named("PStd");
+                EObs1 = Variable.Observed(value, timeInterval).Named("EObs1");
+                EObs2 = Variable.Observed(value, timeInterval).Named("EObs2");
+                QObs = Variable.Observed(value, timeInterval).Named("QObs");
+                QObsVar = Variable.Observed(value, timeInterval).Named("QObsVar");
+                SObs = Variable.Observed(value, timeInterval).Named("SObs");
+                S0Prior = Variable.Observed(s0PriorValue).Named("S0Prior");
+
+                // Water balance variables
+                P = Variable.Array<double>(timeInterval).Named("P");
+                E = Variable.Array<double>(timeInterval).Named("E");
+                Q = Variable.Array<double>(timeInterval).Named("Q");
+                S = Variable.Array<double>(timeInterval).Named("S");
+                S0 = Variable.Random<double, Gaussian>(S0Prior).Named("S0");
+
+                // Time loop
+                using (var time = Variable.ForEach(timeInterval))
+                {
+                    var t = time.Index;
+
+                    // P
+                    var mP = (1 - wP) * PObs1[t] + wP * PObs2[t];
+                    var sP = Variable.Max(PStd[t], rP * 0.5 * Abs(PObs1[t] - PObs2[t]));
+                    P[t] = Variable.GaussianFromMeanAndVariance(mP, sP * sP);
+                    Variable.ConstrainPositive(P[t]);
+
+                    // E
+                    var mE = fE * ((1 - wE) * EObs1[t] + wE * EObs2[t]);
+                    var sE = Variable.Max(0.1 * mE, rE * 0.5 * Abs(EObs1[t] - EObs2[t]));
+                    E[t] = Variable.GaussianFromMeanAndVariance(mE, sE * sE);
+                    Variable.ConstrainPositive(E[t]);
+
+                    // Q
+                    var mQ = Variable.GaussianFromMeanAndVariance(QObs[t], QObsVar[t]);
+                    var sQ = aQ * QObs[t] + bQ;
+                    Q[t] = Variable.GaussianFromMeanAndVariance(mQ, sQ * sQ);
+                    Variable.ConstrainPositive(Q[t]);
+
+                    // S: water balance
+                    using (Variable.If(t == 0))
+                    {
+                        S[t] = S0 + P[t] - E[t] - Q[t];
+                    }
+                    using (Variable.If(t > 0))
+                    {
+                        S[t] = S[t - 1] + P[t] - E[t] - Q[t];
+                    }
+
+                    // SObs
+                    var missingSObs = IsNaN(SObs[t]);
+                    using (Variable.IfNot(missingSObs))
+                    {
+                        const double omega = 2 * Math.PI;
+                        var mS = S[t] + A * Sin(omega * (Variable.Double(t) / 12 - Delta));
+                        var sS = SStd;
+                        SObs[t] = Variable.GaussianFromMeanAndVariance(mS, sS * sS);
+                    }
+                }
+            }
+            SObs.ObservedValue = value;
+            //par.AddAttribute(new PointEstimate());//for optimization (not working atm)
+            par.ObservedValue = parPriorValue.GetMean();//for use with MCMC
+
+            // Generate inference code
+            var engine = new InferenceEngine();
+            engine.OptimiseForVariables = new IVariable[] { P, E, Q, S, S0, evidence, wE, fE, rE, A, Delta, SStd, aQ, bQ, wP, rP };
+            engine.ModelName = "BasinWaterBalance";
+            engine.ModelNamespace = "BasinWaterBalance";
+            engine.Compiler.GeneratedSourceFolder = "../../../";
+            engine.Compiler.WriteSourceFiles = true;
+            engine.Compiler.UseSerialSchedules = true;
+            engine.Compiler.ShowProgress = false;
+            engine.Compiler.OptimiseInferenceCode = true;
+            engine.Infer(evidence);//this line generates the inference code (and runs inference with the dummy input values)
+        }
+
+        // Helper functions
+        private static Variable<bool> IsNaN(Variable<double> x) => Variable<bool>.Factor(double.IsNaN, x);
+        private static Variable<double> Sin(Variable<double> x) => Variable<double>.Factor(Math.Sin, x);
+        private static Variable<double> Abs(Variable<double> x) => Variable<double>.Factor(Math.Abs, x);
+    }
+
+    public static class MCMC
+    {
+        /// <summary>
+        /// Single-chain no-snooker version of the differential evolution MCMC algorithm of ter Braak and Vrugt (2008) to generate samples from a d-dimensional density p(x).
+        /// </summary>
+        /// <remarks>
+        /// Reference: ter Braak, C. J., and Vrugt, J. A. (2008). Differential evolution Markov chain with snooker updater and fewer chains. 
+        /// Statistics and Computing, 18(4), 435-446.
+        /// </remarks>
+        /// <param name="f">Function that computes log-density value of target distribution p(x).</param>
+        /// <param name="N">Number of samples to generate.</param>
+        /// <param name="X">List with 10*d initial values for x. Generated samples will be added to this list.</param>
+        /// <param name="F">List with 10*d corresponding log-density values. Log-density values of samples in X will be added to this list.</param>
+        public static void DEMC_Z(Func<Vector, double> f, int N, List<Vector> X, List<double> F, double probGamma1 = 0.2)
+        {
+            int d = X[0].Count;
+            double normalJump = 2.38 / Math.Sqrt(2 * d);
+            for (int t = 0; t < N; t++)
+            {
+                // STEP 1: generate a new sample using differential evolution
+                // 1a. randomly select two different indices i and j into X (excluding last index)
+                int count = X.Count;
+                int i = Rand.Int(count - 1);//excludes count-1 !
+                int j = i;
+                while (j == i) j = Rand.Int(count - 1);//excludes count-1 !
+
+                // 1b. randomly set jumping rate gamma to 1.0 (10% chance) or to 2.38/sqrt(2*d) (90% chance)
+                double gamma = (Rand.Double() < probGamma1) ? 1 : normalJump;
+
+                // 1c. propose new sample using differential evolution
+                var xOld = X[count - 1];
+                var xNew = Vector.Zero(d);
+                for (int k = 0; k < d; k++) xNew[k] = xOld[k] + gamma * (X[i][k] - X[j][k]) + 0.0001 * Rand.Normal();
+
+                // STEP 2: accept the new sample with probability alpha = min(1, p(xNew)/p(xOld))
+                double fOld = F[count - 1];
+                double fNew = f(xNew);
+                double logAlpha = fNew - fOld;
+                double u = Rand.Double();
+                if (logAlpha > Math.Log(u))//accept the new sample
+                {
+                    X.Add(xNew);
+                    F.Add(fNew);
+                }
+                else//reject (copy the old one)
+                {
+                    xNew.SetTo(xOld);
+                    X.Add(xNew);
+                    F.Add(fOld);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns index of the (first occurrence) of the maximum value in a list of values.
+        /// </summary>
+        /// <param name="values">The values.</param>
+        /// <returns>Index of the (first occurrence) of the maximum value.</returns>
+        public static int IndexOfMax(this IList<double> values)
+        {
+            double maxValue = double.NegativeInfinity;
+            int index = -1;
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (maxValue < values[i])
+                {
+                    maxValue = values[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.ML.Probabilistic.Distributions;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BasinWaterBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment this line to regenerate the EP inference code (only necessary if changes are made to the model)
+            //BasinWaterBalance_EP.GenerateCode();
+
+            // Specify basin folder containing data files
+            if (args.Length == 0) throw new ArgumentException("Missing basin data folder: dotnet run <basinFolder>");
+            string basinFolder = Path.EndsInDirectorySeparator(args[0]) ? args[0] : args[0] + Path.DirectorySeparatorChar;
+
+            // Run analysis
+            InferWaterBalance(basinFolder);
+        }
+
+        private static void InferWaterBalance(string basinFolder)
+        {
+            // Data - missing observations should be stored as double.NaN
+            LoadData(basinFolder, out double[][] PObs, out double[] PStd, out double[][] EObs, out double[] QObs, out double[][] SObs);
+
+            // Estimate monthly QObs distributions
+            var qEst = Util.ArrayInit(12, m => new MeanVarianceAccumulator());
+            for (int t = 0; t < QObs.Length; t++)
+            {
+                int m = t % 12;
+                if (!double.IsNaN(QObs[t])) qEst[m].Add(QObs[t]);
+            }
+
+            // Handle missing QObs = NaN
+            var QObsVar = new double[QObs.Length];
+            for (int t = 0; t < QObs.Length; t++)
+            {
+                if (double.IsNaN(QObs[t]))
+                {
+                    int m = t % 12;
+                    QObs[t] = qEst[m].Mean;
+                    QObsVar[t] = qEst[m].Count == 0 ? double.PositiveInfinity : qEst[m].Variance;
+                }
+            }
+
+            // Prior for transformed par = (wE, fE, rE, A, Delta, SStd, aQ, bQ, fP, fStd)
+            static Gaussian FlatLogitnormal() => new Gaussian(0, 1.4 * 1.4);
+            static Gaussian LognormalFromModeAndCV(double mode, double cv)
+            {
+                double m = Math.Log(mode);
+                double v = MMath.Log1Plus(cv * cv);
+                return Gaussian.FromMeanAndVariance(m, v) * Gaussian.FromNatural(1, 0);
+            }
+            var parPrior = new[]
+            {
+                FlatLogitnormal(),//wE
+                LognormalFromModeAndCV(1, 0.5),//fE
+                FlatLogitnormal(),//rE
+                LognormalFromModeAndCV(30, 2),//A
+                FlatLogitnormal(),//delta
+                LognormalFromModeAndCV(10, 2),//SStd
+                LognormalFromModeAndCV(0.1, 0.01),//aQ, for tight prior use cv=0.01, for vague prior use cv=0.9
+                LognormalFromModeAndCV(0.001, 0.01),//bQ
+                FlatLogitnormal(),//wP
+                FlatLogitnormal() //rP
+            };
+            var priorMean = Vector.Zero(parPrior.Length);
+            var priorVariance = new PositiveDefiniteMatrix(parPrior.Length, parPrior.Length);
+            for (int i = 0; i < parPrior.Length; i++)
+            {
+                priorMean[i] = parPrior[i].GetMean();
+                priorVariance[i, i] = parPrior[i].GetVariance();
+            }
+
+            // Posteriors
+            var wb = new BasinWaterBalance_EP
+            {
+                Nt = PObs[0].Length,
+                PObs1 = PObs[0],
+                PObs2 = PObs[1],
+                PStd = PStd,
+                EObs1 = EObs[0],
+                EObs2 = EObs[1],
+                QObs = QObs,
+                QObsVar = QObsVar,
+                SObs = SObs[0],
+                S0Prior = Gaussian.FromMeanAndVariance(0, 200 * 200),
+                ParPrior = VectorGaussian.FromMeanAndVariance(priorMean, priorVariance)
+            };
+            double maxLogPosterior = wb.InferMCMC();
+            Console.WriteLine($"maxLogPosterior = {maxLogPosterior}");
+
+            // Write results
+            wb.SaveAs($"{basinFolder}results.bin");
+            WriteResult($"{basinFolder}resultsP.out", wb.PMarginal());
+            WriteResult($"{basinFolder}resultsE.out", wb.EMarginal());
+            WriteResult($"{basinFolder}resultsQ.out", wb.QMarginal());
+            WriteResult($"{basinFolder}resultsS.out", wb.SMarginal());
+            WriteResult($"{basinFolder}resultsPar.out", new object[] { wb.wE_marginal_F, wb.fE_marginal_F, wb.rE_marginal_F, wb.A_marginal_F, wb.Delta_marginal_F,
+                                                                       wb.SStd_marginal_F, wb.aQ_marginal_F, wb.bQ_marginal_F, wb.wP_marginal_F, wb.rP_marginal_F });
+        }
+
+        private static void WriteResult<T>(string fileName, IEnumerable<T> items)
+        {
+            using (var file = new StreamWriter(fileName))
+            {
+                foreach (var item in items) file.WriteLine(item.ToString());
+            }
+        }
+
+        private static void LoadData(string basinFolder, out double[][] P, out double[] PStd, out double[][] E, out double[] Q, out double[][] S)
+        {
+            // Read fileNames.txt to get names of the data files
+            var fileNames = new Dictionary<string, string>();
+            using (var file = new StreamReader($"{basinFolder}fileNames.txt"))
+            {
+                char[] delim = { '\t', ' ', ',' };
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine();
+                    var values = line.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                    fileNames.Add(values[0], values[1]);
+                }
+            }
+
+            // Read data files
+            Q = File.ReadAllLines($"{basinFolder}{fileNames["QObs"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            P = new double[2][];
+            P[0] = File.ReadAllLines($"{basinFolder}{fileNames["PObs1"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            P[1] = File.ReadAllLines($"{basinFolder}{fileNames["PObs2"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            PStd = File.ReadAllLines($"{basinFolder}{fileNames["PStd"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            E = new double[2][];
+            E[0] = File.ReadAllLines($"{basinFolder}{fileNames["EObs1"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            E[1] = File.ReadAllLines($"{basinFolder}{fileNames["EObs2"]}").Select(s => double.Parse(s)).ToArray();//mm/month
+            S = new double[1][];
+            S[0] = File.ReadAllLines($"{basinFolder}{fileNames["SObs"]}").Select(s => double.Parse(s)).ToArray();//mm
+
+            // Set missing observations to NaN
+            for (int t = 0; t < P[0].Length; t++)
+            {
+                if (Q[t] < 0) Q[t] = double.NaN;
+                if (P[0][t] < 0) P[0][t] = double.NaN;
+                if (P[1][t] < 0) P[1][t] = double.NaN;
+                if (PStd[t] < 0) PStd[t] = double.NaN;
+                if (E[0][t] < 0) E[0][t] = double.NaN;
+                if (E[1][t] < 0) E[1][t] = double.NaN;
+                if (S[0][t] == -99999) S[0][t] = double.NaN;
+            }
+        }
+    }
+}
